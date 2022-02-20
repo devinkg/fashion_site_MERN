70ba3f58beddeecba85ba97a95f6a381cdf00f61
Devin Godage <devinkanilka@gmail.com>
2022-02-21 00:19:39 +0530

    open product page when clicking product search button and parsing product id with it.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { FavoriteBorderOutlined, SearchOffOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Info = styled.div`
     opacity: 0;
@@ -73,7 +74,9 @@ const Product = ({item}) => {
                     <ShoppingCartOutlined/>
                 </Icon>
                 <Icon>
-                    <SearchOffOutlined/>
+                    <Link to={`/product/${item._id}`}>
+                        <SearchOffOutlined />
+                    </Link>
                 </Icon>
                 <Icon>
                     <FavoriteBorderOutlined/>
