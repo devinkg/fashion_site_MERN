3d7944995d8f8c89072a41ac84651823d7dc20df
Devin Godage <devinkanilka@gmail.com>
2022-02-20 22:58:23 +0530

    product filtering function added

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -31,6 +31,16 @@ const Products = ({ cat, filters, sort }) => {
         getProducts();
     }, [cat])
 
+    useEffect(() => {
+        cat && setFilteredProducts(
+            products.filter((item) =>
+                Object.entries(filters).every(([key, value]) => (
+                    item[key].includes(value)
+                ))
+            )
+        );
+    }, [products, cat, filters])
+
     return (
         <Container>
             {popularProducts.map((item) => (
