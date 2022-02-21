9dd767c2d68d156acd7dce4e778a2eb0c7212955
Devin Godage <devinkanilka@gmail.com>
2022-02-22 01:57:23 +0530

    Quantity adding or removing, selecting colors and sizes functions added

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -118,8 +118,11 @@ const Button = styled.button`
 const Product = () => {
     const location = useLocation();
     const id = location.pathname.split("/")[2];
-
     const [product, setProduct] = useState({});
+    const [quantity, setQuantity] = useState(1);
+    const [color, setColor] = useState("");
+    const [size, setSize] = useState("");
+
     useEffect(() => {
         const getProduct = async () => {
             try {
@@ -131,6 +134,18 @@ const Product = () => {
         }
         getProduct();
     }, [id])
+
+    const handleQuantity = (type) => {
+        if (type === "dec") {
+            quantity > 1 && setQuantity(quantity - 1);
+        } else {
+            setQuantity(quantity + 1);
+        }
+    }
+
+    const handleClick = () => {
+        //Update the cart
+    }
 
     return (
         <Container>
@@ -148,12 +163,12 @@ const Product = () => {
                         <Filter>
                             <FilterTitle>Color</FilterTitle>
                             {product?.color?.map((c) => (
-                                <FilterColor color={c} key={c} />
+                                <FilterColor color={c} key={c} onClick={() => setColor(c)} />
                             ))}
                         </Filter>
                         <Filter>
                             <FilterTitle>Size</FilterTitle>
-                            <FilterSize>
+                            <FilterSize onChange={(e) => setSize(e.target.value)}>
                                 {product?.size?.map((s_value) => (
                                     <FilterSizeOption key={s_value}>{s_value?.toUpperCase()}</FilterSizeOption>
                                 ))}
@@ -162,11 +177,11 @@ const Product = () => {
                     </FilterContainer>
                     <AddContainer>
                         <AmountContainer>
-                            <Remove />
-                            <Amount>1</Amount>
-                            <Add />
+                            <Remove onClick={() => handleQuantity("dec")} />
+                            <Amount>{quantity}</Amount>
+                            <Add onClick={() => handleQuantity("inc")} />
                         </AmountContainer>
-                        <Button>ADD TO CART</Button>
+                        <Button onClick={handleClick}>ADD TO CART</Button>
                     </AddContainer>
                 </InfoContainer>
             </Wrapper>
