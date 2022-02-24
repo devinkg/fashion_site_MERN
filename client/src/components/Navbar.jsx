745c4131a874cf341201db9e703a5df68620bd03
Devin Godage <devinkanilka@gmail.com>
2022-02-25 01:31:02 +0530

    NavBar Badge number bind with redux value.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { Badge } from '@mui/material';
 import { Search, ShoppingCartOutlined } from '@mui/icons-material';
 import { mobile } from '../responsive';
+//Redux
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
     height: 60px;
@@ -68,6 +70,7 @@ const MenuItem = styled.div`
 `;
 
 const Navbar = () => {
+    const quantity = useSelector(state => state?.cart?.quantity);
     return (
         <Container>
             <Wrapper>
@@ -85,7 +88,7 @@ const Navbar = () => {
                     <MenuItem>REGISTER</MenuItem>
                     <MenuItem>SIGN IN</MenuItem>
                     <MenuItem>
-                        <Badge badgeContent={10} color="primary">
+                        <Badge badgeContent={quantity} color="primary">
                             <ShoppingCartOutlined  color="action" />
                         </Badge>
                     </MenuItem>
