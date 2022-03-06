abb398520d9d4c773af68a9452cdc3c79e09141a
Devin Godage <devinkanilka@gmail.com>
2022-03-07 00:17:36 +0530

    Redux Persist configured and applied

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export default configureStore({
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+/* REFER (ISSUE: store.dispatch is not a function)
+    https://stackoverflow.com/a/70164070/9516745 
+*/
+
+export const store = configureStore({
     reducer: {
         cart: cartReducer,
-        user: userReducer,
-    }
+        user: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+export let persistor = persistStore(store);
