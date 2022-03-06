bd24c23f50c64e55593b1239f9109a6eb03774d9
Devin Godage <devinkanilka@gmail.com>
2022-03-06 21:51:54 +0530

    UI data binding code changes added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { login } from "../redux/apiCalls";
 import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 100vw;
@@ -48,6 +51,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled{
+    color:green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -57,21 +64,41 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color:red ;
+`
+
 const Login = () => {
-    return (
-        <Container>
-            <Wrapper>
-                <Title>SIGN IN</Title>
-                <Form>
-                    <Input placeholder="username" />
-                    <Input placeholder="password" />
-                    <Button>LOGIN</Button>
-                    <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-                    <Link>CREATE A NEW ACCOUNT</Link>
-                </Form>
-            </Wrapper>
-        </Container>
-    );
+  const [username, setUsername] = useState(""); //used userName not userName , beacause to same with db field name (username) 
+  const [password, setpassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector(state => state.user);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password })
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input placeholder="password"
+            type="password"
+            onChange={(e) => setpassword(e.target.value)}
+          />
+          <Button onClick={handleLogin} disabled={isFetching}>LOGIN</Button>
+          {error && <Error>Something went wrong...</Error>}
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Login;
