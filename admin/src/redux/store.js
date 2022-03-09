9f505677f7f1fca45a9797804ad3d44479d6211c
Devin <devinkanilka@gmail.com>
2022-03-10 00:25:26 +0530

    redux added in admin project

---
+++ admin/src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userRedux";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+/* REFER (ISSUE: store.dispatch is not a function)
+    https://stackoverflow.com/a/70164070/9516745 
+*/
+
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+        user: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export let persistor = persistStore(store);
