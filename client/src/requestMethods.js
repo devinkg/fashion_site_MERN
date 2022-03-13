5fc3be0658e23e7e1c08b9ad91cbcdaf32cfaf7b
Devin <devinkanilka@gmail.com>
2022-03-13 14:26:25 +0530

    accesToken geeting from web storage code added

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmFkNGJhZTMzZmUzN2RiYzA0YWMwNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTM4NDA0MiwiZXhwIjoxNjQ1NjQzMjQyfQ.u6tgrIUiJ7iMSPRvzsNNxe71eZAnoysvS1zrAdY_030"
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+    .currentUser.accessToken; //access token getting from web local storage , one of the best ways is using cookies in the video mentioning.
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
