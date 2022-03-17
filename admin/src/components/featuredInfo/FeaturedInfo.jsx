16720114ff25bd5a0ba4f88a4ea47402a0171601
Devin <devinkanilka@gmail.com>
2022-03-17 22:18:30 +0530

    percentage calc added

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,15 +1,40 @@
 import "./featuredInfo.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useState, useEffect } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function FeaturedInfo() {
+  const [income, setIncome] = useState();
+  const [perc, setPerc] = useState(0);
+
+  const getIncome = async () => {
+    try {
+      const res = await userRequest.get("orders/income");
+      setIncome(res.data);
+      const percValue = await calculatePercentage(res.data);
+      setPerc(percValue);
+    } catch (e) { }
+  }
+
+  useEffect(() => {
+    getIncome();
+  }, []);
+
+  const calculatePercentage = async (responseData) => {
+    const lastIncome = responseData[responseData?.length - 1]?.total;
+    const monthBeforeLastIncome = responseData[responseData?.length - 2]?.total;
+    let percentagevalue = Number((lastIncome * 100) / monthBeforeLastIncome - 100)
+    return percentagevalue || 0;
+  }
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revanue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoney">${income && Number(income[income?.length - 1]?.total || 0)}</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownward  className="featuredIcon negative"/>
+            %{perc} <ArrowDownward className="featuredIcon negative" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
@@ -19,7 +44,7 @@ export default function FeaturedInfo() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">$4,415</span>
           <span className="featuredMoneyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative"/>
+            -1.4 <ArrowDownward className="featuredIcon negative" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
@@ -29,7 +54,7 @@ export default function FeaturedInfo() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">$2,225</span>
           <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon"/>
+            +2.4 <ArrowUpward className="featuredIcon" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
