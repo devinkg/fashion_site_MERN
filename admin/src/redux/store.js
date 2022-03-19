b4b92a7b4c2485f7a1e2806fd9b79cfd6298f095
Devin <devinkanilka@gmail.com>
2022-03-19 10:43:40 +0530

    productReducer added to the redux store

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userRedux";
+import productReducer from "./productRedux";
 import {
     persistStore,
     persistReducer,
@@ -18,7 +19,7 @@ const persistConfig = {
     storage: storage,
 }
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({ user: userReducer, product: productReducer });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
